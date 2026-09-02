df68992cb79639deb4c08a7a49da2bec46b8548f
Uriel Luna <uriel.tsuki@gmail.com>
2026-09-02 15:37:23 -0600

    correccion de errores v6

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 from flask_login import LoginManager
 from flask_login import login_user
